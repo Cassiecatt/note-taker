840aa65eae4a16a8c3bb8e0e8b00b6d01d04723a
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-08 13:32:34 -0700

    added api post route

--- Develop/server.js
+++ Develop/server.js
@@ -2,31 +2,48 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path')
+const db = require('./db/db.json');
+const fs = require('fs');
 
+//Empty notes array to push into to
+let notes = [];
 
-
-//parse incoming string or array data
+//parse incoming string, array or JSON data
 app.use(express.urlencoded({ extended: true }));
-//parse incoming json data
 app.use(express.json());
-//parse files within public
 app.use(express.static('./public'));
 
-//get route that returns index.html file
+//HTML Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
-//get route that returns notes.html file
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-//post route
-app.post('/notes', (req, res) => {
-    res.send("successful")
-
+//API Routes
+app.get('/api/notes', (req, res) => {
+    return res.json(db);
 })
+
+//API Post Route
+app.post('/api/notes', (req, res) => {
+    //push new note into db.json
+    let newNote = req.body;
+    db.push(newNote);
+
+    //Write to db file
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(db));
+    return res.json(newNote);
+});
+
+//HTML post route
+app.post('/notes', (req, res) => {
+
+    res.send(req.body)
+
+});
 
 //port listener
 app.listen(PORT, () => {
